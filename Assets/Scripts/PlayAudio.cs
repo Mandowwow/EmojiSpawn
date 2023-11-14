207860b1a226ca090a552dbbbec0c64f3e756ec9
Setrak Farahian <setoferahian@gmail.com>
2023-11-14 21:54:48 +0100

    Add Music
    Add PlayAudio.cs to handle music.
    Add music files

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private AudioSource source;
+    private bool musicStart = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0) {
+            if (musicStart) {
+                source.PlayOneShot(clip);
+                musicStart = false;
+            }
+        }
+    }
+}
